830f6bfe181808f9b1b4d0709fa9a30eebec9c0e
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-02-15 17:22:51 +0300

    FileUtils DownloadFileTest

---
+++ src/main/java/FileUtils.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class FileUtils {
+    public static String generateMD5(File file) throws Exception {
+        return hashFile(file, "MD5");
+    }
+
+    public static String generateSHA1(File file) throws Exception {
+        return hashFile(file, "SHA-1");
+    }
+
+    public static String generateSHA256(File file) throws Exception {
+        return hashFile(file, "SHA-256");
+    }
+
+    private static String hashFile(File file, String algorithm)
+            throws Exception {
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+
+            byte[] bytesBuffer = new byte[1024];
+            int bytesRead = -1;
+
+            while ((bytesRead = inputStream.read(bytesBuffer)) != -1) {
+                digest.update(bytesBuffer, 0, bytesRead);
+            }
+
+            byte[] hashedBytes = digest.digest();
+
+            return convertByteArrayToHexString(hashedBytes);
+        } catch (NoSuchAlgorithmException | IOException ex) {
+            throw new Exception("Could not generate hash from file", ex);
+        }
+    }
+
+    private static String convertByteArrayToHexString(byte[] arrayBytes) {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < arrayBytes.length; i++) {
+            stringBuffer.append(Integer.toString((arrayBytes[i] & 0xff) + 0x100, 16)
+                    .substring(1));
+        }
+        return stringBuffer.toString();
+    }
+}
---
+++ src/test/java/DownloadFileTest.java
@@ -0,0 +1,92 @@
+import net.lightbody.bmp.proxy.ProxyServer;
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpResponseInterceptor;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DownloadFileTest {
+    @Test
+    public void downloadingFilesTest() throws Exception{
+        ProxyServer bmp = new ProxyServer(8072);
+        bmp.start();
+
+        HttpResponseInterceptor downloader = new FileDownloader()
+                .addContentType("image/jpeg");
+        bmp.addResponseInterceptor(downloader);
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(CapabilityType.PROXY, bmp.seleniumProxy());
+
+        WebDriver driver = new FirefoxDriver(caps);
+
+        driver.get("http://the-internet.herokuapp.com");
+        driver.findElement(By.linkText("File Download")).click();
+        driver.findElement(By.linkText("avatar.jpg")).click();
+
+        File downloadedFile = new File(driver.findElement(By.tagName("body"))
+                .getText());
+        System.out.println(downloadedFile);
+        System.out.println(FileUtils.generateMD5(downloadedFile));
+        Assert.assertTrue(downloadedFile.exists());
+        Assert.assertEquals(FileUtils.generateMD5(downloadedFile), "d56900c1544ce6a413d1e0dee32e98fc");
+
+        Thread.sleep(8000);
+
+        driver.quit();
+        bmp.stop();
+    }
+
+    public static class FileDownloader implements HttpResponseInterceptor {
+
+        private Set<String> contentTypes = new HashSet<>();
+        private File tempDir = null;
+        private File tempFile = null;
+
+        public FileDownloader addContentType(String contentType) {
+            contentTypes.add(contentType);
+
+            return this;
+        }
+
+
+        @Override
+        public void process(HttpResponse response, HttpContext context) throws HttpException, IOException {
+            String contentType = response.getFirstHeader("Content-Type").getValue();
+            if(contentTypes.contains(contentType)) {
+                String postfix = contentType.substring(contentType.indexOf('/') + 1);
+                tempFile = File.createTempFile("downloaded", "." + postfix, tempDir);
+                tempFile.deleteOnExit();
+
+                FileOutputStream outputStream = new FileOutputStream(tempFile);
+                outputStream.write(EntityUtils.toByteArray(response.getEntity()));
+                outputStream.close();
+
+                response.removeHeaders("Content-Type");
+                response.removeHeaders("Content-Encoding");
+                response.removeHeaders("Content-Disposition");
+                response.removeHeaders("Content-Length");
+
+                response.addHeader("Content-Type", "text/html");
+                response.addHeader("Content-Length", "" +tempFile.getAbsolutePath().length());
+                response.setEntity(new StringEntity(tempFile.getAbsolutePath()));
+            }
+        }
+    }
+}
+
+
